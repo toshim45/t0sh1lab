6d319944d6cb37d7c5628c27e9a253d0bc57317b
artikow <artikow@gmail.com>
2023-09-30 16:54:37 +0700

    grpc updated

---
+++ grpc-lab/go/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pbEmpty "github.com/golang/protobuf/ptypes/empty"
+	pbMsg "github.com/toshim45/grpclab/messaging"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":8081"
+)
+
+type MessageService struct {
+	pbMsg.MessageServiceServer
+}
+
+func (ms *MessageService) UpdateMessage(ctx context.Context, req *pbMsg.MessageRequest) (res *pbMsg.MessageResponse, err error) {
+	log.Printf("got text %s\n", req.Label)
+	return &pbMsg.MessageResponse{Label: req.Label, Created: 1000}, nil
+}
+
+func (ms *MessageService) CheckMessage(ctx context.Context, req *pbEmpty.Empty) (res *pbMsg.CheckResponse, err error) {
+	log.Printf("check\n")
+	return &pbMsg.CheckResponse{Status: pbMsg.ServingStatus_SERVING}, nil
+}
+
+func (ms *MessageService) ListServingStatus(ctx context.Context, req *pbEmpty.Empty) (res *pbMsg.MapServingStatusResponse, err error) {
+	return &pbMsg.MapServingStatusResponse{
+		Status: pbMsg.ServingStatus_name,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pbMsg.RegisterMessageServiceServer(s, &MessageService{})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
